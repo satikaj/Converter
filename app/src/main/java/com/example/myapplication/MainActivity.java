cc728c1c807be8ed7015b4906cfaeb404da7d826
satikaj <117552851+satikaj@users.noreply.github.com>
2023-03-31 11:47:27 +1100

    Implement conversion logic and reorganise UI

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,9 @@ import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private ActivityMainBinding binding;
+    private String unitType;
+    private int fromUnitPos;
+    private int toUnitPos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,26 +26,45 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         unitTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         binding.spnUnitType.setAdapter(unitTypeAdapter);
         binding.spnUnitType.setOnItemSelectedListener(this);
+
+        binding.btnConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double input = Double.parseDouble(binding.edtInput.getText().toString());
+                double result;
+                if (unitType.equals("Temperature")) result = UnitConverter.convertTemp(fromUnitPos, toUnitPos, input);
+                else result = UnitConverter.convert(unitType, fromUnitPos, toUnitPos, input);
+                binding.txtResult.setText(String.format("%.2f", result));
+            }
+        });
     }
 
     private void setUnitSpinnerItems(int unitArrayId) {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, unitArrayId, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         binding.spnFromUnit.setAdapter(adapter);
+        binding.spnFromUnit.setOnItemSelectedListener(this);
         binding.spnToUnit.setAdapter(adapter);
+        binding.spnToUnit.setOnItemSelectedListener(this);
     }
 
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
         if (parent == binding.spnUnitType) {
+            unitType = parent.getItemAtPosition(pos).toString();
             switch(pos) {
                 case 0:
-                    setUnitSpinnerItems(R.array.length_units); break;
+                    setUnitSpinnerItems(R.array.length_units);
+                    break;
                 case 1:
-                    setUnitSpinnerItems(R.array.weight_units); break;
+                    setUnitSpinnerItems(R.array.weight_units);
+                    break;
                 case 2:
-                    setUnitSpinnerItems(R.array.temp_units); break;
+                    setUnitSpinnerItems(R.array.temp_units);
+                    break;
             }
         }
+        if (parent == binding.spnFromUnit) fromUnitPos = pos;
+        if (parent == binding.spnToUnit) toUnitPos = pos;
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
---
+++ app/src/main/java/com/example/myapplication/UnitConverter.java
@@ -0,0 +1,54 @@
+package com.example.myapplication;
+
+public class UnitConverter {
+    private static final double[][] lengthConversionValues = {
+            {1, 1/2.54, 1/30.48, 1/91.44, 1/100000.0, 1/160900.0},
+            {2.54, 1, 1/12.0, 1/36.0, 1/39370.0, 1/63360.0},
+            {30.48, 12, 1, 1/3.0, 1/3281.0, 1/5280.0},
+            {91.44, 36, 3, 1, 1/1094.0, 1/1760.0},
+            {100000, 39370, 3281, 1094, 1, 1/1.609},
+            {160900, 63360, 5280, 1760, 1.609, 1}
+    };
+
+    private static final double[][] weightConversionValues = {
+            {1, 1/1000.0, 1/453.6, 1/28.35, 1/907200.0},
+            {1000, 1, 2.205, 35.274, 1/907.2},
+            {453.6, 1/2.205, 1, 16, 1/2000.0},
+            {28.3495, 1/35.274, 1/16.0, 1, 1/32000.0},
+            {907185, 907.185, 2000, 32000, 1}
+    };
+
+    public static double convert(String unitType, int fromUnitIdx, int toUnitIdx, Double input) {
+        double conversionValue = 0;
+        if (unitType.equals("Length")) conversionValue = lengthConversionValues[fromUnitIdx][toUnitIdx];
+        if (unitType.equals("Weight")) conversionValue = weightConversionValues[fromUnitIdx][toUnitIdx];
+        return input * conversionValue;
+    }
+
+    public static double convertTemp(int fromUnitIdx, int toUnitIdx, double input) {
+        final int CELSIUS = 0, FAHRENHEIT = 1, KELVIN = 2;
+        if (fromUnitIdx == CELSIUS && toUnitIdx == FAHRENHEIT) return celsiusToFahrenheit(input);
+        if (fromUnitIdx == FAHRENHEIT && toUnitIdx == CELSIUS) return fahrenheitToCelsius(input);
+        if (fromUnitIdx == CELSIUS && toUnitIdx == KELVIN) return celsiusToKelvin(input);
+        if (fromUnitIdx == KELVIN && toUnitIdx == CELSIUS) return kelvinToCelsius(input);
+        if (fromUnitIdx == FAHRENHEIT && toUnitIdx == KELVIN) return celsiusToKelvin(fahrenheitToCelsius(input));
+        if (fromUnitIdx == KELVIN && toUnitIdx == FAHRENHEIT) return kelvinToCelsius(celsiusToFahrenheit(input));
+        return 0;
+    }
+
+    private static double celsiusToFahrenheit(double cel) {
+        return (cel * 1.8) + 32;
+    }
+
+    private static double fahrenheitToCelsius(double fah) {
+        return (fah - 32) / 1.8;
+    }
+
+    private static double celsiusToKelvin(double cel) {
+        return cel + 273.15;
+    }
+
+    private static double kelvinToCelsius(double kel) {
+        return kel - 273.15;
+    }
+}
